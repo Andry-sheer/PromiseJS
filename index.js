9206cd1e4feeec344234ef4f8cc02bc0febcd735
Andry <andry.sheremetew@gmail.com>
2024-06-07 03:47:46 +0300

    fix 3 add catch

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 const buttonSearch = document.querySelector('.containerOne__buttonSearch');
 const containerOne = document.querySelector('.containerOne');
 const containerTwo = document.querySelector('.containerTwo');
@@ -23,13 +22,20 @@ function show(){
 
   if (inputSearch.addEventListener('focusin', ()=> { containerError.style.display = 'none'; }));
 
-  const post = fetch(`https://jsonplaceholder.typicode.com/posts/${Search}`)
-    .then(responsePosts => responsePosts.json());
 
-  const comment = fetch(`https://jsonplaceholder.typicode.com/comments/${Search}`)
-    .then(responseComment => responseComment.json());
+    const post = fetch(`https://jsonplaceholder.typicode.com/posts/${Search}`)
+      .then(responsePosts => responsePosts.json());
 
-      Promise.all([post, comment]).then(data => {
+    const comment = fetch(`https://jsonplaceholder.typicode.com/comments/${Search}`)
+      .then(responseComment => responseComment.json());
+
+      Promise.all([post, comment]).catch(() => {
+        containerError.style.display = 'flex';
+        containerError.textContent = 'Server Error (Catch)';
+        containerTwo.style.display = 'none';
+        containerThree.style.display = 'none';
+      })
+      .then(data => {
 
         const id = document.querySelector('.containerTwo__id');
         const title = document.querySelector('.containerTwo__title');
@@ -46,13 +52,13 @@ function show(){
         title.textContent = `TITLE: ${data[0].title}`;
         userId.textContent = `USER_ID: ${data[0].userId}`;
 
-      buttonComment.addEventListener('click', commenti);
-        function commenti() {
-          containerThree.style.display = 'flex';
-          comment_1.textContent = `COMMENTAR ID: ${data[1].id}`;
-          comment_2.textContent = `${data[1].body}`;
-          comment_3.textContent = `EMAIL: ${data[1].email}`;
-          comment_4.textContent = `NAME: ${data[1].name}`;
+    buttonComment.addEventListener('click', commenti);
+      function commenti() {
+        containerThree.style.display = 'flex';
+        comment_1.textContent = `COMMENTAR ID: ${data[1].id}`;
+        comment_2.textContent = `${data[1].body}`;
+        comment_3.textContent = `EMAIL: ${data[1].email}`;
+        comment_4.textContent = `NAME: ${data[1].name}`;
     }
   })
 };
